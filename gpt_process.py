6b2d1b788c299d5b5d5d8aa04501cba6baa7d68c
Azuma413 <hirekatsu0523@gmail.com>
2023-08-07 08:18:52 +0900

    2023_08_07

--- gpt_process.py
+++ gpt_process.py
@@ -1,9 +1,5 @@
 import openai
 import os
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import String
-from chatrover_msgs.msg import ObjectPosition
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -15,50 +11,24 @@ class ChatGPTClass:
         if len(self.messages)>10:
             del self.messages[0:2]
         # ユーザの回答を保存
-        self.messages += [system_conf, {"role": "user", "content": userMessage}, {"role": "assistant", "content": 'response : '}]
+        self.messages += [system_conf, {"role": "user", "content": userMessage}, {"role": "assistant", "content": "{'action': '"}]
 
         # APIを使ってChatGPTの回答を生成
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=self.messages)
         ai_response = response['choices'][0]['message']['content']
         # AIの回答を保存
-        self.messages[-1]["content"] += ai_response
+        self.messages[-1]["content"] = ai_response
         del self.messages[-3]
         #print(self.messages, flush=True)
         return ai_response
     
-class GPTController(Node):
+gpt = ChatGPTClass()
+result = gpt.chatProcess("commands:赤いボールを取ってきてください\nobject data:{person:[x=410.2515869140625, y=345.660400390625, z=0.0}, {bed:[x=410.2515869140625, y=345.660400390625, z=0.0}, {bed:[x=410.2515869140625, y=345.660400390625, z=0.0}, {chair:[x=410.2515869140625, y=345.660400390625, z=0.0}, {red_ball:[x=410.2515869140625, y=345.660400390625, z=0.0}, {chair:[x=410.2515869140625, y=345.660400390625, z=0.0}, {backpack:[x=410.2515869140625, y=345.660400390625, z=0.0}, ")
+print(result)
 
-    def __init__(self):
-        super().__init__('gpt_controller')
-        self.publisher = self.create_publisher(String, '/gpt_command', 10)
-        self.create_subscription(ObjectPosition, '/object_pos', self.yolo_callback, 10)
-        self.create_subscription(String, '/voice_text', self.vosk_callback, 10)
-        self.detect_object_list = ""
-        self.chat = ChatGPTClass()
 
-    def yolo_callback(self, msg):
-        self.detect_object_list = ""
-        if len(msg.name) == 0:
-            return
-        for i in range(len(msg.name)):
-            self.detect_object_list += "[" + msg.name[i] + ":[x=" + str(msg.position[i].x) + ", y=" + str(msg.position[i].y) + ", z=" + str(msg.position[i].z) +  "], "
 
-    def vosk_callback(self, msg):
-        send_commands = String()
-        input_data = "commands:" + msg.data + "\nobject data:" + self.detect_object_list
-        print(input_data, flush=True)
-        #send_commands.data = self.chat.chatProcess(input_data)
-        #self.publisher.publish(send_commands)
 
-def main(args=None):
-    rclpy.init(args=args)
-    controller = GPTController()
-    rclpy.spin(controller)
-    controller.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
 
 """
 sample
@@ -70,3 +40,4 @@ commands:赤いボールを取ってきてください
 object data:{person:[x=410.2515869140625, y=345.660400390625, z=0.0}, {bed:[x=410.2515869140625, y=345.660400390625, z=0.0}, {bed:[x=410.2515869140625, y=345.660400390625, z=0.0}, {chair:[x=410.2515869140625, y=345.660400390625, z=0.0}, {bed:[x=410.2515869140625, y=345.660400390625, z=0.0}, {chair:[x=410.2515869140625, y=345.660400390625, z=0.0}, {backpack:[x=410.2515869140625, y=345.660400390625, z=0.0}, 
 
 """
+
